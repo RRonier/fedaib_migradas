36dc1e5fb72f956fe3dde670d64014dd70a16a7b
RRonier <ronier2332@gmail.com>
2023-11-07 19:00:36 +0100

    add translations for diferent languages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    i18n: {
+        locales: ['en-US', 'es', 'eu-ES', 'fr-FR'],
+        defaultLocale: 'en-US',
+        localeDetection: true
+      },
+}
 
 module.exports = nextConfig
