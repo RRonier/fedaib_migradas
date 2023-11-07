7ca6761a49c20190abcd1d6ddb2e6f1c377d8163
RRonier <ronier2332@gmail.com>
2023-11-07 19:03:04 +0100

    change pages directory

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+"use client"
+import styles from "./footer.module.css"
+import { useTranslations } from 'next-intl';
+
+export function Footer() {
+    const t = useTranslations('footer')
+
+    return (
+        <footer className={styles.footer}>
+            <p style={{fontSize: 'calc(15px + 0.390625vw)', textTransform: 'uppercase'}}>-{t('message')}-</p>
+        </footer>
+    )
+}
