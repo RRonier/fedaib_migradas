e823b883dbeb0e513edd432acce3ba1f0f48be77
RRonier <ronier2332@gmail.com>
2023-11-27 01:19:56 +0100

    add notFound page

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,17 +1,5 @@
-'use client';
+import {notFound} from 'next/navigation';
 
-import Error from 'next/error';
-
-// Render the default Next.js 404 page when a route
-// is requested that doesn't match the middleware and
-// therefore doesn't have a locale associated with it.
-
-export default function NotFound() {
-    return (
-        <html lang='en'>
-            <body>
-                <Error statusCode={404} />
-            </body>
-        </html>
-    );
+export default function CatchAllPage() {
+  notFound();
 }
