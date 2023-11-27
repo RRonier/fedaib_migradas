676c5dc92109f12dc8e891668152b224e92a1c6d
RRonier <ronier2332@gmail.com>
2023-11-27 02:01:46 +0100

    delete [...rest] folder until export bug is fixed in nextjs

--- src/app/[locale]/[...rest]/page.tsx
+++
@@ -1,9 +0,0 @@
-import {notFound} from 'next/navigation';
-
-export async function generateStaticParams() {
-    return [{ lang: 'en-US' }, { lang: 'de' }]
-  }
-
-export default function CatchAllPage() {
-  notFound();
-}
