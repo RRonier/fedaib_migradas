9edd6bcdd5c18bf9c25042a21710cfffae06f2c3
RRonier <ronier2332@gmail.com>
2023-11-27 01:55:14 +0100

    install latest next version and remove export from config

--- src/app/[locale]/[...rest]/page.tsx
+++ src/app/[locale]/[...rest]/page.tsx
@@ -1,5 +1,9 @@
 import {notFound} from 'next/navigation';
 
+export async function generateStaticParams() {
+    return [{ lang: 'en-US' }, { lang: 'de' }]
+  }
+
 export default function CatchAllPage() {
   notFound();
 }
